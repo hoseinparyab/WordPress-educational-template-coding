69148cd190dc5999edd4dfc49a930ca50828a879
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2026-01-27 23:58:03 +0330

    create new metabox for upload video

--- functions.php
+++ functions.php
@@ -1,110 +1,46 @@
 <?php
 
 
-function add_theme_scripts(){
+function add_theme_scripts()
+{
     // اضافه کردن فایل های css
-    wp_enqueue_style('all' , get_template_directory_uri() . '/css/all.css' , array() , false , 'all');
-    wp_enqueue_style('owl.carousel' , get_template_directory_uri() . '/css/owl.carousel.min.css' , array() , false , 'all');
-    wp_enqueue_style('owl.theme' , get_template_directory_uri() . '/css/owl.theme.default.min.css' , array() , false , 'all');
-    wp_enqueue_style('style' , get_stylesheet_uri() , array() , false , 'all');
+    wp_enqueue_style('all', get_template_directory_uri() . '/css/all.css', array(), false, 'all');
+    wp_enqueue_style('owl.carousel', get_template_directory_uri() . '/css/owl.carousel.min.css', array(), false, 'all');
+    wp_enqueue_style('owl.theme', get_template_directory_uri() . '/css/owl.theme.default.min.css', array(), false, 'all');
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), false, 'all');
 
     //اضافه کردن فایل جاوااسکریپت
-    wp_enqueue_script('jq' , get_template_directory_uri() . '/js/jquery-3.5.1.min.js' , array() , false , true);
-    wp_enqueue_script('owl.carousel.js' , get_template_directory_uri() . '/js/owl.carousel.min.js' , array('jquery') , false , true);
-    wp_enqueue_script('main' , get_template_directory_uri() . '/js/main.js' , array('jquery') , false , true);
+    wp_enqueue_script('jq', get_template_directory_uri() . '/js/jquery-3.5.1.min.js', array(), false, true);
+    wp_enqueue_script('owl.carousel.js', get_template_directory_uri() . '/js/owl.carousel.min.js', array('jquery'), false, true);
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), false, true);
 }
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
 
 
 
-function pishro_setup_theme(){
+function pishro_setup_theme()
+{
     add_theme_support('title-tag');
     add_theme_support('automatic-feed-links');
-    add_theme_support('post-thumbnails');  
-    add_image_size('article','313','181',true);
+    add_theme_support('post-thumbnails');
+    add_image_size('article', 313, 181, true);
+
     register_nav_menus(
         array(
-            'main-menu' => __( 'جایگاه فهرست اصلی ' ),
-            'top-menu' => __( 'جایگاه فهرست بالای سایت' )
+            'main-menu' => __('جایگاه فهرست اصلی '),
+            'top-menu' => __('جایگاه فهرست بالای سایت')
         )
     );
 }
-add_action('after_setup_theme','pishro_setup_theme');
+add_action('after_setup_theme', 'pishro_setup_theme');
 
 
-function costum_expert_length(){
-    return 20;
-}
-add_filter('excerpt_length','costum_expert_length',999);
-
-
-
-function post_type_pishro_tv()
+function custop_excerpt_length()
 {
-    $labels = array(
-        'name'               => __('وبسایت آموزشی'),
-        'singular_name'      => __('وبسایت آموزشی'),
-        'menu_name'          => __('وبسایت آموزشی'),
-        'name_admin_bar'     => __('وبسایت آموزشی'),
-        'add_new'            => __(' افزودن جدید'),
-        'add_new_item'       => __('پست مخصوص ویدیوهای آموزشی'),
-        'new_item'           => __('پست جدید'),
-        'edit_item'          => __('ویرایش پست'),
-        'view_item'          => __('مشاهده پست'),
-        'all_items'          => __('همه ویدیوها'),
-        'search_items'       => __('جستجو در بین ویدیوها'),
-        'parent_item_colon'  => __('مادر'),
-        'not_found'          => __('مطلب یافت نشد'),
-        'not_found_in_trash' => __('مطلب در زباله دان یافت نشد')
-    );
-    $args = array(
-        'labels'             => $labels,
-        'description'        => __('Description.', 'your-plugin-textdomain'),
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-
-        'capability_type'    => 'post',
-        'has_archive'        => true,
-        'hierarchical'       => false,
-        'menu_position'      => null,
-        'taxonomies' => array('post_tag'),
-        //'taxonomies' => array('post_tag'),
-        'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')
-    );
-    register_post_type('tv', $args);
+    return 25;
 }
-add_action('init', 'post_type_pishro_tv');
+add_filter('excerpt_length', 'custop_excerpt_length', 999);
 
 
-
-// اضافه کردن تاکسونومی برای پست تایپ وبسافتTV
-function create_taxonomies_for_tv()
-{
-    $labels = array(
-        'name'              => _x('دسته بندی', 'دسته بندی'),
-        'singular_name'     => _x('دسته بندی پست ها ', 'دسته بندی'),
-        'search_items'      => __('جستجویه دسته'),
-        'all_items'         => __('تمام دسته ها'),
-        'parent_item'       => __('زیر دسته'),
-        'parent_item_colon' => __('Parent Genre:'),
-        'edit_item'         => __('ویرایش دسته'),
-        'update_item'       => __('بروزرسانی دسته'),
-        'add_new_item'      => __('افزودن دسته جدید'),
-        'new_item_name'     => __('نام جدید دسته'),
-        'menu_name'         => __('دسته بندی'),
-    );
-
-    $ar = array(
-        'hierarchical'      => true,
-        'labels'            => $labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-    );
-
-    register_taxonomy('cat_pishro_tv', 'tv', $ar);
-}
-add_action('init', 'create_taxonomies_for_tv');
+require_once 'inc/tv-posttype.php';
+require_once 'inc/video-tv.php';
