4c446287653ff0ec264e0b73416599434b09fd5e
hoseinparyab <hoseinparyab1@gmail.com>
2026-02-03 23:24:26 +0330

    Update file permissions and add archive and sidebar templates
    - Changed file permissions for multiple files from 644 to 755.
    - Added new archive.php template for displaying archive pages.
    - Introduced sidebar.php with recent posts widget.
    - Updated TV link in template to use dynamic archive link.

---
+++ sidebar.php
@@ -0,0 +1,33 @@
+        <div class="sidebar">
+            <div class="single-widget">
+                <h4>آخرین مطالب</h4>
+                <ul>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                </ul>
+            </div>
+
+            <div class="single-widget">
+                <h4>آخرین مطالب</h4>
+                <ul>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                    <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
+                </ul>
+            </div>
+        </div>  
