1431a76824e143aaff94b418827ab84b86fbe6df
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2026-02-08 17:52:13 +0330

    Add widgets

--- functions.php
+++ functions.php
@@ -1,44 +1,61 @@
 <?php
 
 
-function add_theme_scripts(){
+function add_theme_scripts()
+{
     // اضافه کردن فایل های css
-    wp_enqueue_style('all' , get_template_directory_uri() . '/css/all.css' , array() , false , 'all');
-    wp_enqueue_style('owl.carousel' , get_template_directory_uri() . '/css/owl.carousel.min.css' , array() , false , 'all');
-    wp_enqueue_style('owl.theme' , get_template_directory_uri() . '/css/owl.theme.default.min.css' , array() , false , 'all');
-    wp_enqueue_style('style' , get_stylesheet_uri() , array() , false , 'all');
+    wp_enqueue_style('all', get_template_directory_uri() . '/css/all.css', array(), false, 'all');
+    wp_enqueue_style('owl.carousel', get_template_directory_uri() . '/css/owl.carousel.min.css', array(), false, 'all');
+    wp_enqueue_style('owl.theme', get_template_directory_uri() . '/css/owl.theme.default.min.css', array(), false, 'all');
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), false, 'all');
 
     //اضافه کردن فایل جاوااسکریپت
-    wp_enqueue_script('jq' , get_template_directory_uri() . '/js/jquery-3.5.1.min.js' , array() , false , true);
-    wp_enqueue_script('owl.carousel.js' , get_template_directory_uri() . '/js/owl.carousel.min.js' , array('jquery') , false , true);
-    wp_enqueue_script('main' , get_template_directory_uri() . '/js/main.js' , array('jquery') , false , true);
+    wp_enqueue_script('jq', get_template_directory_uri() . '/js/jquery-3.5.1.min.js', array(), false, true);
+    wp_enqueue_script('owl.carousel.js', get_template_directory_uri() . '/js/owl.carousel.min.js', array('jquery'), false, true);
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), false, true);
 }
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
 
 
 
-function pishro_setup_theme(){
+function pishro_setup_theme()
+{
     add_theme_support('title-tag');
     add_theme_support('automatic-feed-links');
     add_theme_support('post-thumbnails');
-    add_image_size('article',313,181,true);
-    add_image_size('tv_large',820,548,true);
-    add_image_size('tv_small',265,165,true);
+    add_image_size('article', 313, 181, true);
+    add_image_size('tv_large', 820, 548, true);
+    add_image_size('tv_small', 265, 165, true);
 
-    register_nav_menus( 
+    register_nav_menus(
         array(
-            'main-menu' => __( 'جایگاه فهرست اصلی ' ),
-            'top-menu' => __( 'جایگاه فهرست بالای سایت' )
+            'main-menu' => __('جایگاه فهرست اصلی '),
+            'top-menu' => __('جایگاه فهرست بالای سایت')
         )
     );
 }
-add_action('after_setup_theme','pishro_setup_theme');
+add_action('after_setup_theme', 'pishro_setup_theme');
 
 
-function custop_excerpt_length() {
+function pishro_widget()
+{
+    register_sidebar(array(
+        'name'          => __('ناحیه کناری بلاگ'),
+        'id'            => 'pishro_blog',
+
+        'before_widget' => '<div class="single-widget">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ));
+}
+add_action('widgets_init', 'pishro_widget');
+
+function custop_excerpt_length()
+{
     return 25;
 }
-add_filter('excerpt_length' , 'custop_excerpt_length',999);
+add_filter('excerpt_length', 'custop_excerpt_length', 999);
 
 
 require_once 'inc/tv-posttype.php';
--- sidebar.php
+++ sidebar.php
@@ -1,33 +1,7 @@
 <div class="sidebar">
-    <div class="single-widget">
-        <h4>آخرین مطالب</h4>
-        <ul>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-        </ul>
-    </div>
 
-    <div class="single-widget">
-        <h4>آخرین مطالب</h4>
-        <ul>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-            <li><a href="#" target="_blank">پیشنیاز php چیست ؟ پیش نیاز های برنامه نویسی</a> </li>
-        </ul>
-    </div>
+    <?php if (is_active_sidebar('pishro_blog')) {
+        dynamic_sidebar('pishro_blog');
+    }
+    ?>
 </div>
